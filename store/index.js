604b70914a03894e067b7545258e6447c9e8eee7
fupan1018 <1194927471@qq.com>
2019-06-20 09:10:00 +0800

    Add files via upload

---
+++ store/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+    state: {
+		GWsearchData : '',// 搜索页面保存的参数
+		pageUrl:'',//页面地址
+		calendar:''//日期
+		
+    },
+	getters:{
+		getPageUrl:state=>state.pageUrl,
+		getCalendar:state=>state.calendar
+	},
+    mutations: {
+        setPageUrl(state,Url){
+			//更改状态--页面地址
+			state.pageUrl=Url;
+		},
+		setCalendar(state,calendar){
+			//设置日期
+			state.calendar=calendar;
+		}
+    },
+	actions:{
+		
+	}
+})
+
+export default store
